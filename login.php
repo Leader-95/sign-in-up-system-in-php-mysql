d7035020ea9fc4ff4aa3164e17de59bbf2501a52
Batyr Velsahdow <111680970+Leader-95@users.noreply.github.com>
2023-10-27 17:56:28 +0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LogIn</title>
+</head>
+
+<body>
+    <form action="includes/login.inc.php" method="post">
+        <input type="email" name='email' placeholder="Enter email"><br>
+        <input type="password" name='pwd' placeholder="Enter password"><br>
+        <input type="submit" name='submit' value="Login">
+    </form>
+    <a href="signup.php">SignUp</a><br>
+    <a href="index.php">HOME</a>
+</body>
+
+</html>
